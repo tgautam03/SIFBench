639a17e898f73f39decddd06d0a51c7b5b564e4c
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2025-05-05 15:10:22 -0600

    Updated

---
+++ drivers/rfr_test.py
@@ -0,0 +1,111 @@
+import numpy as np
+import pandas as pd
+from joblib import dump, load
+from tqdm import tqdm
+
+import itertools
+import os
+
+def mean_normalized_l2(y_true, y_pred):
+    return np.mean(np.linalg.norm(y_true -  y_pred, ord=2, axis=1) / np.linalg.norm(y_true, ord=2, axis=1))
+
+if __name__ == "__main__":
+    # SINGLE or TWIN
+    which_data = "SINGLE"
+    
+    # Specify the directory path
+    dir_path = "../files/data/FINAL_CSV/{}/TEST".format(which_data)
+
+    # Get all .csv files
+    csv_files = [f for f in os.listdir(dir_path) if f.endswith(".csv")]
+
+    l2_errs = np.zeros((len(csv_files), 3))
+
+    count = 0
+    for csv_file in tqdm(csv_files):
+        print("=============================")
+        print("========= Testing: {} ========".format(csv_file))
+        print("=============================")
+        # Surface crack only has tension loading
+        if csv_file[:7] == "SURFACE":
+            df = pd.read_csv(dir_path + "/{}".format(csv_file))
+            test_combinations = df.iloc[:, 1:4].drop_duplicates().to_numpy()
+
+            # Drop crack index
+            d = df.to_numpy()[::8,1:]
+            print("Dataset Size: ", d.shape)
+
+            # X, y
+            print("---------------")
+            print("Tension Loading")
+            print("---------------")
+
+            # Testing
+            rfr = load('../files/trained_models/rfr/{}_TENSION.joblib'.format(csv_file[:-4].replace("TEST", "TRAIN")))
+            y_pred = rfr.predict(d[:,:-1])
+
+            # Saving predictions model
+            np.save('../files/predictions/rfr/{}_TENSION.npy'.format(csv_file[:-4]), y_pred)
+            l2_err = mean_normalized_l2(d[:,-1].reshape(len(test_combinations), 16), y_pred.reshape(len(test_combinations), 16))
+            print("L2 error: ", l2_err)
+
+            l2_errs[count, 0] = l2_err
+            count += 1
+
+        else:
+            df = pd.read_csv(dir_path + "/{}".format(csv_file))
+            test_combinations = df.iloc[:, 1:5].drop_duplicates().to_numpy()
+
+            # Drop crack index
+            d = df.to_numpy()[::8,1:]
+            print("Dataset Size: ", d.shape)
+
+            # X, y
+            print("---------------")
+            print("Tension Loading")
+            print("---------------")
+
+            # Testing
+            rfr = load('../files/trained_models/rfr/{}_TENSION.joblib'.format(csv_file[:-4].replace("TEST", "TRAIN")))
+            y_pred = rfr.predict(d[:,:-1])
+
+            # Saving predictions model
+            np.save('../files/predictions/rfr/{}_TENSION.npy'.format(csv_file[:-4]), y_pred)
+            l2_err = mean_normalized_l2(d[:,-3].reshape(len(test_combinations), 16), y_pred.reshape(len(test_combinations), 16))
+            print("L2 error: ", l2_err)
+
+            l2_errs[count, 0] = l2_err
+
+            print("---------------")
+            print("Bending Loading")
+            print("---------------")
+            
+            # Testing
+            rfr = load('../files/trained_models/rfr/{}_BENDING.joblib'.format(csv_file[:-4].replace("TEST", "TRAIN")))
+            y_pred = rfr.predict(d[:,:-1])
+
+            # Saving predictions model
+            np.save('../files/predictions/rfr/{}_BENDING.npy'.format(csv_file[:-4]), y_pred)
+            l2_err = mean_normalized_l2(d[:,-2].reshape(len(test_combinations), 16), y_pred.reshape(len(test_combinations), 16))
+            print("L2 error: ", l2_err)
+
+            l2_errs[count, 1] = l2_err
+
+            print("---------------")
+            print("Bearing Loading")
+            print("---------------")
+            
+            # Testing
+            rfr = load('../files/trained_models/rfr/{}_BEARING.joblib'.format(csv_file[:-4].replace("TEST", "TRAIN")))
+            y_pred = rfr.predict(d[:,:-1])
+
+            # Saving predictions model
+            np.save('../files/predictions/rfr/{}_BEARING.npy'.format(csv_file[:-4]), y_pred)
+            l2_err = mean_normalized_l2(d[:,-1].reshape(len(test_combinations), 16), y_pred.reshape(len(test_combinations), 16))
+            print("L2 error: ", l2_err)
+
+            l2_errs[count, 2] = l2_err
+            
+            count += 1
+
+    np.save("../files/metrics/l2_err_rfr.npy", l2_errs)
---
+++ drivers/rfr_train.py
@@ -0,0 +1,107 @@
+import numpy as np
+import pandas as pd
+from joblib import dump, load
+from tqdm import tqdm
+
+from sklearn.ensemble import RandomForestRegressor
+
+from sklearn.model_selection import train_test_split
+
+import itertools
+import os
+
+def shuffle_arrays(arr1, arr2):
+    assert len(arr1) == len(arr2)
+    permutation = np.random.permutation(len(arr1))
+    return arr1[permutation], arr2[permutation]
+
+if __name__ == "__main__":
+    # SINGLE or TWIN
+    which_data = "SINGLE"
+    
+    # Specify the directory path
+    dir_path = "../files/data/FINAL_CSV/{}/TRAIN".format(which_data)
+
+    # Get all .csv files
+    csv_files = [f for f in os.listdir(dir_path) if f.endswith(".csv")]
+
+    for csv_file in tqdm(csv_files):
+        print("=============================")
+        print("========= Training: {} ========".format(csv_file))
+        print("=============================")
+        # Surface crack only has tension loading
+        if csv_file[:7] == "SURFACE":
+            df = pd.read_csv(dir_path + "/{}".format(csv_file))
+
+            # Drop crack index
+            d = df.to_numpy()[:,1:]
+            print("Dataset Size: ", d.shape)
+            print("Training on a subset of size 100000 (randomly sampled)")
+            random_indices = np.random.choice(len(d), size=100000, replace=False)
+            d = d[random_indices]
+            print("Revised dataset Size: ", d.shape)
+
+            # X, y
+            print("---------------")
+            print("Tension Loading")
+            print("---------------")
+            X_train, y_train = shuffle_arrays(d[:,:-1], d[:,-1])
+
+            # Train
+            rfr = RandomForestRegressor(max_depth=None)
+            rfr.fit(X_train, y_train)
+
+            # Saving trained model
+            dump(rfr, '../files/trained_models/rfr/{}_TENSION.joblib'.format(csv_file[:-4]))
+            print("")
+
+        else:
+            df = pd.read_csv(dir_path + "/{}".format(csv_file))
+
+            # Drop crack index
+            d = df.to_numpy()[:,1:]
+            print("Dataset Size: ", d.shape)
+            print("Training on a subset of size 100000 (randomly sampled)")
+            random_indices = np.random.choice(len(d), size=100000, replace=False)
+            d = d[random_indices]
+            print("Revised dataset Size: ", d.shape)
+
+            # X, y
+            print("---------------")
+            print("Tension Loading")
+            print("---------------")
+            X_train, y_train = shuffle_arrays(d[:,:-1], d[:,-3])
+
+            # Train
+            rfr = RandomForestRegressor(max_depth=None)
+            rfr.fit(X_train, y_train)
+
+            # Saving trained model
+            dump(rfr, '../files/trained_models/rfr/{}_TENSION.joblib'.format(csv_file[:-4]))
+            print("")
+
+            print("---------------")
+            print("Bending Loading")
+            print("---------------")
+            X_train, y_train = shuffle_arrays(d[:,:-1], d[:,-2])
+
+            # Train
+            rfr = RandomForestRegressor(max_depth=None)
+            rfr.fit(X_train, y_train)
+
+            # Saving trained model
+            dump(rfr, '../files/trained_models/rfr/{}_BENDING.joblib'.format(csv_file[:-4]))
+            print("")
+
+            print("---------------")
+            print("Bearing Loading")
+            print("---------------")
+            X_train, y_train = shuffle_arrays(d[:,:-1], d[:,-1])
+
+            # Train
+            rfr = RandomForestRegressor(max_depth=None)
+            rfr.fit(X_train, y_train)
+
+            # Saving trained model
+            dump(rfr, '../files/trained_models/rfr/{}_BEARING.joblib'.format(csv_file[:-4]))
+            print("")
