f034642e3b4dbd07217d017b86a9ced85fd7eceb
tgautam03 <30533136+tgautam03@users.noreply.github.com>
2025-05-12 18:17:02 -0600

    Updated error code and readme

---
+++ ERRS.py
@@ -0,0 +1,331 @@
+import numpy as np
+import pandas as pd
+
+def mean_normalized_l2(y_true, y_pred):
+    return np.mean(np.linalg.norm(y_true -  y_pred, ord=2, axis=1) / np.linalg.norm(y_true, ord=2, axis=1))
+
+def mean_normalized_abs(y_true, y_pred):
+    return np.mean(np.abs((y_true -  y_pred)/y_true))
+
+if __name__ == "__main__":
+    ###########################################################################
+    ############################## SINGLE CRACK ###############################
+    ###########################################################################
+    SINGLE_CRACK_DATASET_LOCS = ['files/data/SINGLE_CRACK/SURFACE_CRACK/SURFACE_CRACK',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_STRAIGHT_HOLE/CORNER_CRACK_BH_QUARTER_ELLIPSE',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_STRAIGHT_HOLE/CORNER_CRACK_BH_THROUGH_THICKNESS',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS1_QUARTER_ELLIPSE',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS2_QUARTER_ELLIPSE',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS2_THROUGH_CS_THICKNESS',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS3_QUARTER_ELLIPSE',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS4_QUARTER_ELLIPSE',
+                                 'files/data/SINGLE_CRACK/CORNER_CRACK_COUNTERSUNK_HOLE/CORNER_CRACK_CS4_THROUGH_CS_THICKNESS']
+    
+    LOADINGS = ['TENSION', 'BENDING', 'BEARING']
+
+    single_l2 = np.zeros((len(SINGLE_CRACK_DATASET_LOCS), 3, 4))
+    single_abs = np.zeros((len(SINGLE_CRACK_DATASET_LOCS), 3, 4))
+
+    for (i, data_name) in enumerate(SINGLE_CRACK_DATASET_LOCS):
+        # Extract the last part of the path
+        crack_type = data_name.split('/')[-1]
+        
+        if crack_type == "SURFACE_CRACK":
+            df_test = pd.read_csv(data_name+'_TEST.csv')
+            test_combinations = df_test.iloc[:, 1:4].drop_duplicates().to_numpy()
+            d = df_test.to_numpy()[:,1:]
+
+            # RFR
+            y_test = d[:,-1].reshape(-1, 128)
+            y_pred = np.load('files/predictions/rfr/{}_TENSION.npy'.format(crack_type)).reshape(-1, 128)
+            single_l2[i,0,0] = mean_normalized_l2(y_test, y_pred)
+            single_abs[i,0,0] = mean_normalized_abs(y_test, y_pred)
+
+            # SVR
+            y_test = d[:,-1].reshape(-1, 128)
+            y_pred = np.load('files/predictions/svr/{}_TENSION.npy'.format(crack_type)).reshape(-1, 128)
+            single_l2[i,0,1] = mean_normalized_l2(y_test, y_pred)
+            single_abs[i,0,1] = mean_normalized_abs(y_test, y_pred)
+
+            # NN
+            y_test = d[:,-1].reshape(-1, 128)
+            y_pred = np.load('files/predictions/nn/{}_TENSION.npy'.format(crack_type)).reshape(-1, 128)
+            single_l2[i,0,2] = mean_normalized_l2(y_test, y_pred)
+            single_abs[i,0,2] = mean_normalized_abs(y_test, y_pred)
+
+            # FNO
+            y_test = d[:,-1].reshape(-1, 128)
+            y_pred = np.load('files/predictions/fno/{}_TENSION.npy'.format(crack_type))
+            single_l2[i,0,3] = mean_normalized_l2(y_test, y_pred)
+            single_abs[i,0,3] = mean_normalized_abs(y_test, y_pred)
+        
+        elif crack_type == "CORNER_CRACK_BH_THROUGH_THICKNESS":
+            df_test = pd.read_csv(data_name+'_TEST.csv')
+            df_test = df_test.drop(columns=['b/t'])
+            df_test = df_test[(df_test["r/t"] >= 0.5) & (df_test["r/t"] <= 1.5)]
+            test_combinations = df_test.iloc[:, 1:5].drop_duplicates().to_numpy()
+            d = df_test.to_numpy()[:,1:]
+
+            for (j, LOADING) in enumerate(LOADINGS):
+                # RFR
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/rfr/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,0] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,0] = mean_normalized_abs(y_test, y_pred)
+
+                # SVR
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/svr/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,1] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,1] = mean_normalized_abs(y_test, y_pred)
+
+                # NN
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/nn/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,2] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,2] = mean_normalized_abs(y_test, y_pred)
+
+                # FNO
+                y_test = d[:,-3+j].reshape(-1, 128)
+                y_pred = np.load('files/predictions/fno/{}_{}.npy'.format(crack_type, LOADING))
+                single_l2[i,j,3] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,3] = mean_normalized_abs(y_test, y_pred)
+
+        else:
+            df_test = pd.read_csv(data_name+'_TEST.csv')
+            df_test = df_test.drop(columns=['b/t'])
+            df_test = df_test[(df_test["W/R"] >= 2) & (df_test["W/R"] <= 20)]
+            df_test = df_test[(df_test["r/t"] >= 0.5) & (df_test["r/t"] <= 1.5)]
+            test_combinations = df_test.iloc[:, 1:5].drop_duplicates().to_numpy()
+            d = df_test.to_numpy()[:,1:]
+
+            for (j, LOADING) in enumerate(LOADINGS):
+                # RFR
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/rfr/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,0] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,0] = mean_normalized_abs(y_test, y_pred)
+
+                # SVR
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/svr/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,1] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,1] = mean_normalized_abs(y_test, y_pred)
+
+                # NN
+                y_test = d[::8,-3+j].reshape(-1, 16)
+                y_pred = np.load('files/predictions/nn/{}_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                single_l2[i,j,2] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,2] = mean_normalized_abs(y_test, y_pred)
+
+                # FNO
+                y_test = d[:,-3+j].reshape(-1, 128)
+                y_pred = np.load('files/predictions/fno/{}_{}.npy'.format(crack_type, LOADING))
+                single_l2[i,j,3] = mean_normalized_l2(y_test, y_pred)
+                single_abs[i,j,3] = mean_normalized_abs(y_test, y_pred)
+
+    print("----------------------------------------------------------")
+    print("----------------- SINGLE CRACK L2 ERROR ------------------")
+    print("----------------------------------------------------------")
+    for (i, data_name) in enumerate(SINGLE_CRACK_DATASET_LOCS):
+        # Extract the last part of the path
+        crack_type = data_name.split('/')[-1]
+        if crack_type == "SURFACE_CRACK":
+            print("{} (TENSION) | {:.4f} & {:.4f} & {:.4f} & {:.4f}".format(crack_type, 
+                                                  single_l2[i,0,0],
+                                                  single_l2[i,0,1],
+                                                  single_l2[i,0,2],
+                                                  single_l2[i,0,3]))
+            print("----------------------------------------------------------")
+        else:
+            for (j, LOADING) in enumerate(LOADINGS):
+                print("{} ({}) | {:.4f} & {:.4f} & {:.4f} & {:.4f}".format(crack_type, LOADING,
+                                                      single_l2[i,j,0],
+                                                      single_l2[i,j,1],
+                                                      single_l2[i,j,2],
+                                                      single_l2[i,j,3]))
+                
+                if LOADING == "BEARING":
+                    print("----------------------------------------------------------")
+
+
+    print("")
+
+    print("----------------------------------------------------------")
+    print("---------------- SINGLE CRACK ABS ERROR ------------------")
+    print("----------------------------------------------------------")
+    for (i, data_name) in enumerate(SINGLE_CRACK_DATASET_LOCS):
+        # Extract the last part of the path
+        crack_type = data_name.split('/')[-1]
+        if crack_type == "SURFACE_CRACK":
+            print("{} (TENSION) | {:.4f} & {:.4f} & {:.4f} & {:.4f}".format(crack_type, 
+                                                  single_abs[i,0,0],
+                                                  single_abs[i,0,1],
+                                                  single_abs[i,0,2],
+                                                  single_abs[i,0,3]))
+            print("----------------------------------------------------------")
+        else:
+            for (j, LOADING) in enumerate(LOADINGS):
+                print("{} ({}) | {:.4f} & {:.4f} & {:.4f} & {:.4f}".format(crack_type, LOADING,
+                                                      single_abs[i,j,0],
+                                                      single_abs[i,j,1],
+                                                      single_abs[i,j,2],
+                                                      single_abs[i,j,3]))
+                
+                if LOADING == "BEARING":
+                    print("----------------------------------------------------------")
+
+    print("")
+    ###########################################################################
+    ############################### TWIN CRACK ################################
+    ###########################################################################
+    TWIN_CRACK_DATASET_LOCS = ['files/data/TWIN/CORNER_CRACK_STRAIGHT_HOLE/TWIN_CORNER_CRACK_BH_QUARTER_ELLIPSE',
+                               'files/data/TWIN/CORNER_CRACK_STRAIGHT_HOLE/TWIN_CORNER_CRACK_BH_THROUGH_THICKNESS',
+                               'files/data/TWIN/CORNER_CRACK_COUNTERSUNK_HOLE/TWIN_CORNER_CRACK_CS2_QUARTER_ELLIPSE_PART_1',
+                               'files/data/TWIN/CORNER_CRACK_COUNTERSUNK_HOLE/TWIN_CORNER_CRACK_CS2_THROUGH_THICKNESS']
+    
+    LOADINGS = ['TENSION', 'BENDING', 'BEARING']
+
+    TWIN_l2 = np.zeros((len(TWIN_CRACK_DATASET_LOCS), 3, 4, 2))
+    TWIN_abs = np.zeros((len(TWIN_CRACK_DATASET_LOCS), 3, 4, 2))
+
+    for (i, data_name) in enumerate(TWIN_CRACK_DATASET_LOCS):
+        for (j, LOADING) in enumerate(LOADINGS):
+            # Extract the last part of the path
+            crack_type = data_name.split('/')[-1]
+            
+            if crack_type == "TWIN_CORNER_CRACK_BH_QUARTER_ELLIPSE" or crack_type == "TWIN_CORNER_CRACK_BH_THROUGH_THICKNESS":
+                df_test = df_test.drop(columns=['b/t'])
+                df_test = df_test[(df_test["r/t"] >= 0.5) & (df_test["r/t"] <= 1.5)]
+                test_combinations = df_test.iloc[:, 1:7].drop_duplicates().to_numpy()
+                d = df_test.to_numpy()[:,1:]
+
+                # RFR
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/rfr/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,0,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,0,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/rfr/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,0,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,0,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # SVR
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/svr/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,1,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,1,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/svr/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,1,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,1,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # NN
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/nn/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,2,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,2,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/nn/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,2,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,2,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # FNO
+                y1_test = d[:,8 + j*2].reshape(-1, 128)
+                y1_pred = np.load('files/predictions/fno/{}_C1_{}.npy'.format(crack_type, LOADING))
+                TWIN_l2[i,j,3,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,3,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[:,8 + j*2 + 1].reshape(-1, 128)
+                y2_pred = np.load('files/predictions/fno/{}_C2_{}.npy'.format(crack_type, LOADING))
+                TWIN_l2[i,j,3,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,3,1] = mean_normalized_abs(y2_test, y2_pred)
+            
+            else:
+                df_test = df_test.drop(columns=['b/t'])
+                df_test = df_test[(df_test["W/R"] >= 4) & (df_test["W/R"] <= 10)]
+                df_test = df_test[(df_test["r/t"] >= 0.5) & (df_test["r/t"] <= 1.5)]
+                test_combinations = df_test.iloc[:, 1:7].drop_duplicates().to_numpy()
+                d = df_test.to_numpy()[:,1:]
+
+                # RFR
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/rfr/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,0,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,0,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/rfr/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,0,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,0,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # SVR
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/svr/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,1,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,1,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/svr/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,1,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,1,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # NN
+                y1_test = d[::8,8 + j*2].reshape(-1, 16)
+                y1_pred = np.load('files/predictions/nn/{}_C1_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,2,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,2,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[::8,8 + j*2 + 1].reshape(-1, 16)
+                y2_pred = np.load('files/predictions/nn/{}_C2_{}.npy'.format(crack_type, LOADING)).reshape(-1, 16)
+                TWIN_l2[i,j,2,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,2,1] = mean_normalized_abs(y2_test, y2_pred)
+
+                # FNO
+                y1_test = d[:,8 + j*2].reshape(-1, 128)
+                y1_pred = np.load('files/predictions/fno/{}_C1_{}.npy'.format(crack_type, LOADING))
+                TWIN_l2[i,j,3,0] = mean_normalized_l2(y1_test, y1_pred)
+                TWIN_abs[i,j,3,0] = mean_normalized_abs(y1_test, y1_pred)
+
+                y2_test = d[:,8 + j*2 + 1].reshape(-1, 128)
+                y2_pred = np.load('files/predictions/fno/{}_C2_{}.npy'.format(crack_type, LOADING))
+                TWIN_l2[i,j,3,1] = mean_normalized_l2(y2_test, y2_pred)
+                TWIN_abs[i,j,3,1] = mean_normalized_abs(y2_test, y2_pred)
+
+    print("----------------------------------------------------------")
+    print("----------------- TWIN CRACK L2 ERROR ------------------")
+    print("----------------------------------------------------------")
+    for (i, data_name) in enumerate(TWIN_CRACK_DATASET_LOCS):
+        # Extract the last part of the path
+        crack_type = data_name.split('/')[-1]
+        for (j, LOADING) in enumerate(LOADINGS):
+            print("{} ({}) | {:.3f} & {:.3f}   & {:.3f} & {:.3f}   & {:.3f} & {:.3f}   & {:.3f} & {:.3f}".format(crack_type, LOADING,
+                                                                                 TWIN_l2[i,j,0,1], TWIN_l2[i,j,0,2],
+                                                                                 TWIN_l2[i,j,1,1], TWIN_l2[i,j,1,2],
+                                                                                 TWIN_l2[i,j,2,1], TWIN_l2[i,j,2,2],
+                                                                                 TWIN_l2[i,j,3,1], TWIN_l2[i,j,3,2]))
+            
+            if LOADING == "BEARING":
+                print("----------------------------------------------------------")
+
+
+    print("")
+
+    print("----------------------------------------------------------")
+    print("---------------- TWIN CRACK ABS ERROR ------------------")
+    print("----------------------------------------------------------")
+    for (i, data_name) in enumerate(TWIN_CRACK_DATASET_LOCS):
+        # Extract the last part of the path
+        crack_type = data_name.split('/')[-1]
+        for (j, LOADING) in enumerate(LOADINGS):
+            print("{} ({}) | {:.3f} & {:.3f}   & {:.3f} & {:.3f}   & {:.3f} & {:.3f}   & {:.3f} & {:.3f}".format(crack_type, LOADING,
+                                                                                 TWIN_abs[i,j,0,1], TWIN_abs[i,j,0,2],
+                                                                                 TWIN_abs[i,j,1,1], TWIN_abs[i,j,1,2],
+                                                                                 TWIN_abs[i,j,2,1], TWIN_abs[i,j,2,2],
+                                                                                 TWIN_abs[i,j,3,1], TWIN_abs[i,j,3,2]))
+            
+            if LOADING == "BEARING":
+                print("----------------------------------------------------------")
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # SIFBench
-SIF benchmarks
+SIFBench, a benchmark database developed to support the advancement and validation of machine learning methods for stress intensity factor (SIF) prediction. The database contains approximately 5 million unique configurations of cracks, geometries, and loading conditions, organized into 37 distinct scenarios. These scenarios are divided into single-crack and twin-crack cases, comprising roughly 1.6 million and 3.3 million geometries, respectively.
+
+
+The single-crack category includes semi-elliptical surface cracks in a finite plate subjected to tensile loading, as well as various quarter-elliptical and through-thickness corner cracks located at straight and countersunk bolt holes. Most configurations are analyzed under three independent loading conditions: ***tension***, ***bending***, and ***bearing***. The twin-crack category consists of paired quarter-elliptical and through-thickness corner cracks, also positioned at straight and countersunk bolt holes and evaluated under the same three loading conditions, each applied separately.
+
+## Directory Details
+1. **files:** Data, models, predictions files. All these can be accessed from https://huggingface.co/datasets/rvn03/SIFBench/tree/main
+2. **nb_analysis:** Jupyter notebooks showing the detailed dataset analysis.
+3. **src:** Source files for training FNN and FNO. 
+
+## Results
+### Mean Normalized L2 Error
+
+
+### Mean Normalized Absolute Error
+
+## Reproducing Results
+
+### Single Crack
+0. **Surface Crack in a Plate:** 
+    - ***Training:*** `python 00_SURFACE_CRACK.py Train`
+    - ***Testing:*** `python 00_SURFACE_CRACK.py Test`
+1. **Quarter-Elliptical Corner Crack at the straight hole in a Plate:** 
+    - ***Training:*** `python 01_CORNER_CRACK_BH_QE.py Train`
+    - ***Testing:*** `python 01_CORNER_CRACK_BH_QE.py Test`
+2. **Through-Thickness Corner Crack at the straight hole in a Plate:** 
+    - ***Training:*** `python 02_CORNER_CRACK_BH_Th.py Train`
+    - ***Testing:*** `python 02_CORNER_CRACK_BH_Th.py Test`
+3. **Quarter-Elliptical Corner Crack at the countersunk hole (with $b/t=0.75$) in a Plate:** 
+    - ***Training:*** `python 03_CORNER_CRACK_CS1_QE.py Train`
+    - ***Testing:*** `python 03_CORNER_CRACK_CS1_QE.py Test`
+4. **Quarter-Elliptical Corner Crack at the countersunk hole (with $b/t=0.5$) in a Plate:** 
+    - ***Training:*** `python 04_CORNER_CRACK_CS2_QE.py Train`
+    - ***Testing:*** `python 04_CORNER_CRACK_CS2_QE.py Test`
+5. **Through-Thickness Corner Crack at the countersunk hole (with $b/t=0.5$) in a Plate:** 
+    - ***Training:*** `python 05_CORNER_CRACK_CS2_Th.py Train`
+    - ***Testing:*** `python 05_CORNER_CRACK_CS2_Th.py Test`
+6. **Quarter-Elliptical Corner Crack at the countersunk hole (with $b/t=0.25$) in a Plate:** 
+    - ***Training:*** `python 06_CORNER_CRACK_CS3_QE.py Train`
+    - ***Testing:*** `python 06_CORNER_CRACK_CS3_QE.py Test`
+7. **Quarter-Elliptical Corner Crack at the countersunk hole (with $b/t=0.05$) in a Plate:** 
+    - ***Training:*** `python 07_CORNER_CRACK_CS4_QE.py Train`
+    - ***Testing:*** `python 07_CORNER_CRACK_CS4_QE.py Test`
+8. **Through-Thickness Corner Crack at the countersunk hole (with $b/t=0.05$) in a Plate:** 
+    - ***Training:*** `python 08_CORNER_CRACK_CS4_Th.py Train`
+    - ***Testing:*** `python 08_CORNER_CRACK_CS4_Th.py Test`
+
+### Twin Crack
+9. **Twin Quarter-Elliptical Corner Cracks at the straight hole in a Plate:** 
+    - ***Training:*** `python 09_TWIN_CORNER_CRACK_BH_QE.py Train`
+    - ***Testing:*** `python 09_TWIN_CORNER_CRACK_BH_QE.py Test`
+10. **Twin Through-Thickness Corner Crack at the straight hole in a Plate:** 
+    - ***Training:*** `python 10_TWIN_CORNER_CRACK_BH_Th.py Train`
+    - ***Testing:*** `python 10_TWIN_CORNER_CRACK_BH_Th.py Test`
+11. **Twin Quarter-Elliptical Corner Crack at the countersunk hole (with $b/t=0.5$) in a Plate:** 
+    - ***Training:*** `python 11_TWIN_CORNER_CRACK_CS2_QE.py Train`
+    - ***Testing:*** `python 11_TWIN_CORNER_CRACK_CS2_QE.py Test`
+12. **Twin Through-Thickness Corner Crack at the countersunk hole (with $b/t=0.5$) in a Plate:** 
+    - ***Training:*** `python 12_TWIN_CORNER_CRACK_CS2_Th.py Train`
+    - ***Testing:*** `python 12_TWIN_CORNER_CRACK_CS2_Th.py Test`
